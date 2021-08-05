2d9494e2464528c83a9154a5769328dcffd137f1
TamHajiy <78784747+TamHajiy@users.noreply.github.com>
2021-08-05 09:13:52 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -209,7 +209,7 @@ form.addEventListener('submit', (e)=>{
         e.preventDefault();
     }
     //if selected option is credit card, else it wont prevent other options
-    if(e.target === paymentMethod){
+    if(e.target){
     if(!isCardNumValid()){
         e.preventDefault();
     }
